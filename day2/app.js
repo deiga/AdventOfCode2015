160e27c4559ea914a7494cf787fb367af057dd30
Timo Sand <timo.j.sand@gmail.com>
2015-12-07 21:31:47 +0200

    Solves 2nd half of day2

--- day2/app.js
+++ day2/app.js
@@ -1,6 +1,8 @@
 var readline = require('readline');
 
-var feet = 0;
+var wrappingPaper = 0;
+var ribbon = 0;
+
 var rl = readline.createInterface({
   input: require('fs').createReadStream('./day2/input.txt')
 });
@@ -10,8 +12,9 @@ rl.on('line', function (line) {
   var length = measurements[0];
   var width = measurements[1];
   var height = measurements[2];
-  feet += feetOfWrappingPaper(length, width, height);
-  console.log(feet);
+  wrappingPaper += feetOfWrappingPaper(length, width, height);
+  ribbon += feetOfRibbon(length, width, height);
+  console.log(wrappingPaper, ribbon);
 })
 
 function surfaceOfBox(l, w, h) {
@@ -19,10 +22,10 @@ function surfaceOfBox(l, w, h) {
 }
 
 function feetOfWrappingPaper(l, w, h) {
-  return surfaceOfBox(l,w,h) + smallestSide(l,w,h);
+  return surfaceOfBox(l,w,h) + smallestAreaOfSide(l,w,h);
 }
 
-function smallestSide(l, w, h) {
+function smallestAreaOfSide(l, w, h) {
   var bot = l*w;
   var side = w*h;
   var front = h*l;
@@ -34,5 +37,18 @@ function smallestSide(l, w, h) {
     return front;
   }
 }
+
+function feetOfRibbon(l, w, h) {
+  return smallesPerimeter([l, w, h]) + l*w*h;
+}
+
+function smallesPerimeter(measurements) {
+  return measurements.sort(function (a, b) { return a - b; }).splice(0,2).reduce(function (sum, item) { return sum + 2*item; }, 0)
+}
 console.log(feetOfWrappingPaper(2,3,4), 58);
 console.log(feetOfWrappingPaper(1,1,10), 43);
+
+console.log(feetOfRibbon(2,3,4), 34);
+console.log(feetOfRibbon(1,1,10), 14);
+console.log(feetOfRibbon(1,2,10), 26);
+console.log(feetOfRibbon(1,3,10), 38);
